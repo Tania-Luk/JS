4fa8c5439a101ac84005758e809cd5ce2e9f3820
tanluk1010@gmail.com <tanluk1010@gmail.com>
2025-02-05 16:01:30 +0200

    Завершила hw

---
+++ Lesson 1/main2.js
@@ -0,0 +1,9 @@
+let firstName='Тетяна';
+console.log(firstName);
+let middleName='Юріївна';
+console.log(middleName);
+let lastName='Лукьянчук';
+console.log(lastName);
+// let person=`${firstName} ${middleName} ${lastName}`;
+let person=firstName+' '+middleName+' '+lastName;
+console.log(person);
