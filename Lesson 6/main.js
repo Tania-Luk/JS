67e68cc6e856e790e72c20e04c65179633e39729
tanluk1010@gmail.com <tanluk1010@gmail.com>
2025-02-18 11:47:59 +0200

    Завершила hw Lesson 6

--- Lesson 6/main.js
+++ Lesson 6/main.js
@@ -98,7 +98,6 @@
 //     .sort((a, b) => b.monthDuration - a.monthDuration)
 //     .filter(value => value.monthDuration > 5)
 //     .map((value, index) => ({...value, id: index + 1}));
-//
 // console.log(map1);
 // // =========================
 //     #bolvdlhP
@@ -114,8 +113,30 @@
 //         value: '', // '6'-'10', 'ace','jack','queen','king'
 //     color:'', // 'red','black'
 // }
-//
-// =========================
+const suits=['spade', 'diamond','heart', 'clubs'];
+const values=['6','7','8','9','10', 'jack','queen','king','ace'];
+const cards=[]
+for (const suit of suits){
+    for(const value of values){
+        const card = { cardSuit: suit, value: value}
+        if (suit === 'heart' || suit ==='diamond' ){
+            card.color = 'red'
+        } else(suit === 'spade' || suit === 'clubs' )
+        {
+            card.color = 'black'
+        }
+        cards.push(card);
+    }
+}
+// console.log(cards);
+// console.log(cards.find (card=> card.value === 'ace' && card.cardSuit === 'spade'));
+// console.log(cards.filter (card => card.value === '6'));
+// console.log(cards.filter (card => card.color === 'red'));
+// console.log(cards.filter (card => card.cardSuit === 'diamond'));
+// console.log(cards.filter(card => card.cardSuit === 'clubs' && (card.value !== '6' || card.value !== '7' || card.value !== '8' || card.value !== '9')));
+
+
+//=========================
 //
 //     #EP5I1UUzAX
 // Взяти описану колоду карт, та за допомоги reduce упакувати всі карти по "мастях" в об'єкт
@@ -125,8 +146,102 @@
 //     hearts:[],
 //     clubs:[]
 // }
+// const reduce= cards.reduce((accum, card) => {
+//     switch  (card.cardSuit){
+//         case 'spade':
+//             accum.spades.push(card);
+//             break;
+//         case 'diamond':
+//             accum.diamonds.push(card);
+//             break;
+//         case 'heart':
+//             accum.hearts.push(card);
+//             break;
+//         case 'clubs':
+//             accum.clubs.push(card);
+//             break;
+//     }
+//         return accum;
+// }, {
+//     spades:[],
+//     diamonds:[],
+//     hearts:[],
+//     clubs:[]
+// })
+// console.log(reduce)
 // =========================
 // #4LJn7zBx
 // взяти з arrays.js масив coursesArray
 // --написати пошук всіх об'єктів, в яких в modules є sass
 // --написати пошук всіх об'єктів, в яких в modules є docker
+let coursesArray = [
+    {
+        title: 'JavaScript Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Java Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'java core',
+            'java advanced']
+    },
+    {
+        title: 'Python Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'python core',
+            'python advanced']
+    },
+    {
+        title: 'QA Complex',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+    },
+    {
+        title: 'FullStack',
+        monthDuration: 7,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'react',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'node.js',
+            'python',
+            'java']
+    },
+    {
+        title: 'Frontend',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+    }
+];
+console.log(coursesArray.filter(course => course.modules.includes ('sass')));
+console.log(coursesArray.filter(course => course.modules.includes ('docker')))
