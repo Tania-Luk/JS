3d93e7b5d0cee5800ab2374df68db49f13165908
tanluk1010@gmail.com <tanluk1010@gmail.com>
2025-02-20 12:09:12 +0200

    Створила hw Lesson 8

---
+++ Lesson 8/main.js
@@ -0,0 +1,37 @@
+// #AiN5CoUQ
+// Створити функцію, яка робить глибоку копію об'єкту.
+// Додати перевірки на undefined, null, NaN.
+//     Подумати і реалізувати логіку, в якій кінцевий об'єкт буде мати функції,які в нього були до цього моменту.
+let course = {
+        title: 'JavaScript ',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js'],
+        greeting (){
+            console.log(this);
+        }
+}
+let courseCopy=JSON.stringify(course);
+console.log(courseCopy)
+if (course.monthDuration === 0){
+    console.log ('null');
+} else if (course.monthDuration === ''){
+    console.log('NaN');
+} else if (course.monthDuration !== 5){
+    console.log('undefined');
+}
+
+//
+// #iz6emEsP2BA
+// - є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration
+//     Зробити все ВИКЛЮЧНО за допомоги інлайн конструкції
+//
